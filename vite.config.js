a27c0e84641b2a7e622832eff326fbeaadaf9be9
arhacker848203 <147381572+arhacker848203@users.noreply.github.com>
2026-01-18 09:49:47 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -16,7 +16,7 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "lnmuv2-production.up.railway.app",
+        target: "https://lnmuv2.onrender.com",
         changeOrigin: true,
         rewrite: p => p.replace(/^\/api/, "")
       },
